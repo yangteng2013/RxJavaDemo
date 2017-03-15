d5f3fdd8b00755176192104e79faa04e20958d56
kimi007117 <lj5077929@163.com>
2017-03-16 00:29:26 +0800

    [add] 渐变色按钮和背景

---
+++ app/src/main/java/com/noe/rxjava/GradientActivity.java
@@ -0,0 +1,13 @@
+package com.noe.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gradient);
+    }
+}
