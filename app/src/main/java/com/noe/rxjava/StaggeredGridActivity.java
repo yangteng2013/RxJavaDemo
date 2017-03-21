958ebbbf5b06d348ab3d64d8a8450576a9b2fc00
kimi007117 <lj5077929@163.com>
2017-03-21 15:32:46 +0800

    【add】添加图片圆角处理第三方库

--- app/src/main/java/com/noe/rxjava/StaggeredGridActivity.java
+++ app/src/main/java/com/noe/rxjava/StaggeredGridActivity.java
@@ -12,11 +12,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.noe.rxjava.bean.PersonCard;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
  * Created by lijie24 on 2016/12/2.
@@ -47,24 +49,25 @@ public class StaggeredGridActivity extends AppCompatActivity {
     //生成6个明星数据，这些Url地址都来源于网络
     private List<PersonCard> buildData() {
 
-        String[] names = {"邓紫棋", "范冰冰", "杨幂", "Angelababy", "唐嫣", "柳岩","邓紫棋", "范冰冰", "杨幂", "Angelababy", "唐嫣", "柳岩"};
+        String[] names = {"迪丽热巴", "范冰冰", "杨幂", "Angelababy", "唐嫣", "柳岩", "迪丽热巴", "范冰冰", "杨幂", "Angelababy", "唐嫣", "柳岩","金馆长"};
         String[] imgUrs = {
-                "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1477122728&di=21924aeef8f7847a651fc8bf00a28f49&src=http://www.tengtv.com/file/upload/201609/18/232836341.jpg",
+                "https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3014402647,3821196097&fm=58",
                 "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1477122795&di=f740bd484870f9bcb0cafe454a6465a2&src=http://tpic.home.news.cn/xhCloudNewsPic/xhpic1501/M08/28/06/wKhTlVfs1h2EBoQfAAAAAF479OI749.jpg",
                 "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=673651839,1464649612&fm=111&gp=0.jpg",
                 "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=fd90a83e900a304e4d22a7fae1c9a7c3/d01373f082025aafa480a2f1fcedab64034f1a5d.jpg",
                 "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1849074283,1272897972&fm=111&gp=0.jpg",
                 "https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=005560fc8b5494ee982208191df4e0e1/c2fdfc039245d68827b453e7a3c27d1ed21b243b.jpg",
-                "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1477122728&di=21924aeef8f7847a651fc8bf00a28f49&src=http://www.tengtv.com/file/upload/201609/18/232836341.jpg",
+                "https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3014402647,3821196097&fm=58",
                 "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1477122795&di=f740bd484870f9bcb0cafe454a6465a2&src=http://tpic.home.news.cn/xhCloudNewsPic/xhpic1501/M08/28/06/wKhTlVfs1h2EBoQfAAAAAF479OI749.jpg",
                 "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=673651839,1464649612&fm=111&gp=0.jpg",
                 "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=fd90a83e900a304e4d22a7fae1c9a7c3/d01373f082025aafa480a2f1fcedab64034f1a5d.jpg",
                 "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1849074283,1272897972&fm=111&gp=0.jpg",
-                "https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=005560fc8b5494ee982208191df4e0e1/c2fdfc039245d68827b453e7a3c27d1ed21b243b.jpg"
+                "https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=005560fc8b5494ee982208191df4e0e1/c2fdfc039245d68827b453e7a3c27d1ed21b243b.jpg",
+                "https://gss0.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=cfbcf1b84510b912bf94fef8f3cdd03b/9a504fc2d5628535e770642292ef76c6a7ef6324.jpg"
         };
 
         List<PersonCard> list = new ArrayList<>();
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < names.length; i++) {
             PersonCard p = new PersonCard();
             p.avatarUrl = imgUrs[i];
             p.name = names[i];
@@ -96,7 +99,11 @@ public class StaggeredGridActivity extends AppCompatActivity {
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             MyViewHolder holder2 = (MyViewHolder) holder;
             PersonCard personCard = mData.get(position);
-            Picasso.with(mContext).load(personCard.avatarUrl).into(holder2.userAvatar);
+            if (position % 2 == 0) {
+                Glide.with(mContext).load(personCard.avatarUrl).into(holder2.userAvatar);
+            } else {
+                Glide.with(mContext).load(personCard.avatarUrl).bitmapTransform(new RoundedCornersTransformation(mContext, 10, 0, RoundedCornersTransformation.CornerType.ALL)).into(holder2.userAvatar);
+            }
             holder2.userAvatar.getLayoutParams().height = personCard.imgHeight; //从数据源中获取图片高度，动态设置到控件上
             holder2.userName.setText(personCard.name);
         }
