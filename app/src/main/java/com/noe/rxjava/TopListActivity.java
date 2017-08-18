a917b7f514c4316a467167bbdf7ecf436a6e54d0
kimi007117 <lj5077929@163.com>
2017-08-18 20:27:57 +0800

    【update】list返回顶部

--- app/src/main/java/com/noe/rxjava/TopListActivity.java
+++ app/src/main/java/com/noe/rxjava/TopListActivity.java
@@ -2,7 +2,6 @@ package com.noe.rxjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
